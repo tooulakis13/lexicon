815ea70ba385000ee2469251975604b930649da7
Christodoulos Demetriou <37778768+tooulakis13@users.noreply.github.com>
2018-11-12 14:45:24 +0100

    Update plainLangImport.php

--- mls_lexicon_2/plainLangImport.php
+++ mls_lexicon_2/plainLangImport.php
@@ -1,22 +1,6 @@
 <?php
 global $wpdb;
 
-function testingv1_assign_nums() {
-    global $wpdb;
-    //$langNumRelation = array();
-    $allLanguages = $wpdb->get_results("SELECT * FROM " . _LEXICON_LANGUAGES . ";");
-    $counter = 0;
-    //session_start();
-    foreach ($allLanguages as $language) {
-        $languageId = $language->id;
-        $langNumRelation["$languageId"] = array("All" => $counter,
-            "Untranslated" => $counter + 1,
-            "Waiting" => $counter + 2,
-            "Fuzzy" => $counter + 3);
-        $counter = $counter + 10;
-    }
-    //$_SESSION["giannakis"] = $langNumRelation;
-}
 ?>
 
 <br />
@@ -24,19 +8,25 @@ function testingv1_assign_nums() {
 <?php
 if ('POST' == $_SERVER['REQUEST_METHOD'] && isset($_POST['formSubmit'])) {
     if(isset($_POST['language'])){
-        $aDoor = $_POST['language'];
+        $lexicon_lang = $_POST['language'];
     }else{
-        $aDoor = "";
+        $lexicon_lang = "";
     }
-    if (empty($aDoor)) {
+    if (empty($lexicon_lang)) {
         echo("You didn't select any language.");
     } else {
-        $N = count($aDoor);
+        $N = count($lexicon_lang);
 
-        echo("You selected $N language(s): ");
+        //echo("You selected $N language(s): ");
+        //var_dump($lexicon_lang);
+        foreach ($lexicon_lang as $langToAdd){
+            lexicon_add_language($langToAdd);
+        }
+        echo "All $N languages created successfully!";
+        wp_redirect(esc_url_raw(add_query_arg(array('page' => 'lexicon_testing'), admin_url('admin.php'))));
     }
 } else {
-    $allLanguages = $wpdb->get_results("SELECT * FROM " . _LEXICON_LANGUAGES . " WHERE Part1 <> '' AND Language_Type = 'L';");
+    $allLanguages = $wpdb->get_results("SELECT * FROM " . _LEXICON_LANGUAGES . " WHERE Part1 <> '' AND Language_Type = 'L' AND Status = 'inactive';");
     $counter = 0;
     ?>
     <table>
